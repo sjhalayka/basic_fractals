3e573f07db34a99fae2a8f9a945f99b9ca053bd6
Shawn Halayka <sjhalayka@gmail.com>
2019-06-18 18:27:15 -0600

    Add files via upload

---
+++ 1d_print_trajectory_points.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+
+float iterate_1d(vector<float> &trajectory_points,
+                 float Z,
+                 const float C,
+                 const short unsigned int max_iterations,
+                 const float threshold)
+{
+    trajectory_points.clear();
+    trajectory_points.push_back(Z);
+
+    for (short unsigned int i = 0; i < max_iterations; i++)
+    {
+        Z = Z*Z + C;
+        
+        trajectory_points.push_back(Z);
+        
+        if (sqrtf(Z*Z) >= threshold)
+            break;
+    }
+    
+    return sqrtf(Z*Z);
+}
+
+int main(void)
+{
+    const float x_grid_max = 1.5;
+    const float x_grid_min = -x_grid_max;
+    const size_t x_res = 30;
+    const float x_step_size = (x_grid_max - x_grid_min) / (x_res - 1);
+
+    const float C = 0.2f;
+    const unsigned short int max_iterations = 8;
+    const float threshold = 4.0f;
+    
+    float Z = x_grid_min;
+
+    vector<float> trajectory_points;
+    
+    for (size_t x = 0; x < x_res; x++, Z += x_step_size)
+    {
+        float magnitude = iterate_1d(trajectory_points, Z, C, max_iterations, threshold);
+        
+        if(magnitude < threshold)
+        {
+            for(size_t i = 0; i < trajectory_points.size(); i++)
+            {
+                cout << trajectory_points[i] << endl;
+            }
+        }
+        
+        cout << endl;
+    }
+    
+    cout << endl;
+    
+    return 0;
+}
+
+
+
